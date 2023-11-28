6fa7738d7df6768fb4bf6893a542a0084538bec8
MicaelCadete <micaelcadete07@gmail.com>
2023-11-28 12:29:50 -0300

    Atualizado - 28/11

--- Login/Login.js
+++ Login/Login.js
@@ -1,14 +1,26 @@
-document.addEventListener("DOMContentLoaded", function() {
-    function login(){
-        var user = document.getElementById(user).value;
-        var senha = document.getElementById(senha).value;
+document.addEventListener("DOMContentLoaded", function () {
+    function login() {
+        var fmEmail = document.getElementById("user").value; 
+        var fmSenha = document.getElementById("senha").value;
 
-        if(login == "admin" && senha == "admin123"){
+        var cadastros = JSON.parse(localStorage.getItem("cadastros"));
+
+        var usuarioEncontrado = cadastros.find(function (usuario) {
+            return usuario.email === fmEmail && usuario.senha === fmSenha;
+        });
+
+        if (usuarioEncontrado) {
             alert("Login realizado com sucesso");
-            location.href = "index.html";
-        }else{
-            alert("Usuario ou senha incorretos!");
+            localStorage.setItem("usuarioLogado", JSON.stringify(usuarioEncontrado));
+            window.location.href = "index.html";
+        } else {
+            alert("Usuário ou senha incorretos!");
         }
-
     }
-})
+
+    var btnLogin = document.getElementById("btnLogin");
+    btnLogin.addEventListener("click", function (e) {
+        e.preventDefault(); 
+        login();
+    });
+});
